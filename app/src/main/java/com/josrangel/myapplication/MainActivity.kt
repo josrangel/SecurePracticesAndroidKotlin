3f6d051ec3da74e6947c0045034f22fc73e64e24
josrangel <josrangel@hotmail.com>
2021-10-24 01:54:49 -0500

    - add setFlags FLAG_SECURE - add rooting verify

--- app/src/main/java/com/josrangel/myapplication/MainActivity.kt
+++ app/src/main/java/com/josrangel/myapplication/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.josrangel.myapplication
 
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.josrangel.myapplication.utils.EncriptUtils
+import com.josrangel.myapplication.utils.RootingUtils
+
+/**
+ * Source:
+ * https://stackoverflow.com/questions/38404237/how-to-hide-views-in-android-when-android-app-is-backgrounded-not-to-stop-andro
+ *
+ */
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +24,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makesecurityActivity()
         setContentView(R.layout.activity_main)
+        verifyRooted()
         initIU()
     }
 
@@ -42,5 +53,19 @@ class MainActivity : AppCompatActivity() {
     private fun decriptText() {
         var textDecript = EncriptUtils.decrypt(etEncrypt.text.toString())
         etNormal.setText(textDecript)
+    }
+
+    private fun makesecurityActivity() {
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
+    }
+
+    private fun verifyRooted() {
+        if (RootingUtils.isDeviceRooted()) {
+            Toast.makeText(this, getString(R.string.text_root_detected), Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
