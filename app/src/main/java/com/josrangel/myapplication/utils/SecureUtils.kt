f10c65a7fd87ab4508a1aeb0ff1ded8a04495b19
josrangel <josrangel@hotmail.com>
2021-10-31 23:35:31 -0600

    - add link source

--- app/src/main/java/com/josrangel/myapplication/utils/SecureUtils.kt
+++ app/src/main/java/com/josrangel/myapplication/utils/SecureUtils.kt
@@ -4,7 +4,9 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- * Source: https://stackoverflow.com/questions/38404237/how-to-hide-views-in-android-when-android-app-is-backgrounded-not-to-stop-andro
+ * Source:
+ * https://stackoverflow.com/questions/38404237/how-to-hide-views-in-android-when-android-app-is-backgrounded-not-to-stop-andro
+ * https://medium.com/nomtek/screenshot-preventing-on-mobile-apps-9e62f51643e9
  */
 
 class SecureUtils {
